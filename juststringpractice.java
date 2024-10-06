3bc93086cf45e39647a28d6d0af60f106da00381
ujjawal <ujjawalvishwakarma266@gmail.com>
2024-10-06 12:34:35 +0530

    ujjawal

---
+++ juststringpractice.java
@@ -0,0 +1,15 @@
+public class juststringpractice {
+    public static void main(String[] args){
+        System.out.println("hello ujjawal this is string class which provide intense feature of the java.lang library ");
+        String a="ujjawal";
+        System.out.println(a);
+        String b="ujjawal";
+        if(a.equals(b)){
+            System.out.println("both are same with pointer or string content");
+        }
+        else{
+            System.out.println("no");
+        }
+
+    }
+}
